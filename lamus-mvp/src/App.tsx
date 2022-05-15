868015afbb86fcc482a7fb0030e459d444f8d68a
Jan Starzak <jan.starzak@gmail.com>
2022-05-15 13:38:19 +0200

    feat: automatically focus first text element

--- lamus-mvp/src/App.tsx
+++ lamus-mvp/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Paragraph from '@editorjs/paragraph';
 import Header from '@editorjs/header';
@@ -15,10 +14,22 @@ const ReactEditorJS = createReactEditorJS()
 function App() {
   const blocks: object[] = [];
 
+  const editorCore = React.useRef(null)
+
+  const handleInitialize = React.useCallback((instance: any) => {
+    editorCore.current = instance
+    setTimeout(() => {
+      const mainEl = document.querySelector('.ce-paragraph.cdx-block') as HTMLDivElement
+      if (!mainEl) console.error('Main element not found')
+      mainEl.focus()
+    }, 1000)
+  }, [])
+
   return (
     <div className="App">
       <ReactEditorJS
         defaultValue={blocks}
+        onInitialize={handleInitialize}
         tools={{
           paragraph: Paragraph,
           header: Header,
