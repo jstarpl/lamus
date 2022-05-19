dad0070b95ee015b1315277ca356c3bef3a16e85
Jan Starzak <jan.starzak@gmail.com>
2022-05-19 16:02:36 +0200

    feat: add logo, tweak setup React app

--- setup/src/App.tsx
+++ setup/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Navbar>
+          <Navbar.Brand><img src={logo} alt="Lamus Logo" width={30} height={30} className="d-inline-block align-top" /> Lamus</Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse>
+            <Nav className="me-auto">
+              <Nav.Link>Setup</Nav.Link>
+            </Nav>
+          </Navbar.Collapse> */}
+        </Navbar>
+      </Container>
     </div>
   );
 }
