53892d6d463874073c25757151b419e930c867a5
Jan Starzak <jan.starzak@gmail.com>
2023-11-24 11:55:46 +0100

    chore(Sentry): filter out Lamus API calls

--- lamus-mvp/src/index.tsx
+++ lamus-mvp/src/index.tsx
@@ -11,7 +11,7 @@ Sentry.init({
   integrations: [
     new Sentry.BrowserTracing({
       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      tracePropagationTargets: ["localhost", /^https:\/\/lamus\.cloud\/api/],
     }),
     new Sentry.Replay(),
   ],
